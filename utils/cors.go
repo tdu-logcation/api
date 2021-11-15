c668d1844f0573ddedc854de870597ba4fa42a3f
Yuto Watanabe <yuto.w51942@gmail.com>
2021-11-15 18:09:44 +0900

    corsのAllowedOriginsにループバックアドレス（デバッグ用）を追加

--- utils/cors.go
+++ utils/cors.go
@@ -8,6 +8,8 @@ func CorsConfig() *cors.Cors {
 			"https://log.tdu.app",
 			"https://test-log.tdu.app",
 			"https://local-log.tdu.app",
+			"https://127.0.0.1:3000",
+			"https://localhost:3000",
 		},
 		Debug: false,
 	})
